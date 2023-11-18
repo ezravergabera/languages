61bd90da8c9284d759ca2c73eee502446b4c6255
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-11-18 17:01:54 +0800

    Created a sandbox.sln file, created a praticeProject project, moved the helloworld project inside the sandbox.sln

---
+++ solutions/sandbox/helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+
+//namespace helloworld
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Console.WriteLine("Hello World!");
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+int i = 123;
+object o = i;
+int j = (int)o;
+
+Console.WriteLine(j);
+Console.ReadLine();

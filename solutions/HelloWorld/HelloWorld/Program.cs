aa7518ed38901f8bbd92d6afd237041d73c9e75f
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-12-07 22:29:36 +0800

    Practiced C# data types, Formatted strings, and constants

--- solutions/HelloWorld/HelloWorld/Program.cs
+++ solutions/HelloWorld/HelloWorld/Program.cs
@@ -4,7 +4,28 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //// You can use var instead of data type keywords
+            //byte number = 255;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character = 'a';
+            //string firstName = "Ezra";
+            //bool isWorking = true;
+
+            //Console.Write(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(character);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(isWorking);
+
+            //// Formatted strings
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            //// Constant
+            //const float Pi = 3.1415f;
+            //Pi = 1;
         }
 
     }

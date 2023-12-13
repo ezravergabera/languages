6309fe7d5a75440cb0aa70b5810c0ec623127d58
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-12-13 16:48:36 +0800

    Practiced C# Classes, class members: instance and static

--- solutions/HelloWorld/HelloWorld/Program.cs
+++ solutions/HelloWorld/HelloWorld/Program.cs
@@ -2,8 +2,38 @@
 {
     class Program
     {
+        public class Person
+        {
+            public string Name;
+
+            public void Introduce(string to)
+            {
+                Console.WriteLine("Hello {0}, my name is {1}.", to, Name);
+            }
+
+            public static Person Parse(string str)
+            {
+                var person = new Person();
+                person.Name = str;
+
+                return person;
+            }
+        }
+
         private static void Main(string[] args)
         {
+            // C# classes
+            // making use of instance member
+            //var person = new Person();
+            //person.Name = "Ezra";
+            //person.Introduce("Mosh");
+
+            // making use of static member
+            var person = Person.Parse("Ezra");
+            person.Introduce("Mosh");
+
+
+
             //// You can use var instead of data type keywords
             //byte number = 255;
             //int count = 10;

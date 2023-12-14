0c31a596e28b7b0b3bdde13950b9f8544cfb3f2a
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-12-14 16:46:42 +0800

    Updated .gitignore

--- tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-printf("Jacob => %u\n", hash("Jacob"));
-    // printf("Natalie => %u\n", hash("Natalie"));
-    // printf("Tobogo => %u\n", hash("Tobogo"));
-    // printf("Jose => %u\n", hash("Jose"));
-    // printf("Arjie => %u\n", hash("Arjie"));
-    // printf("Pauline => %u\n", hash("Pauline"));
-    // printf("Joseph => %u\n", hash("Joseph"));
-    // printf("Mary => %u\n", hash("Mary"));
--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-if (q->rear == -1)
-    // {
-    //     q->rear = (q->rear + 1) % MAX;
-    //     q->items[q->rear] = data;
-    //     return;
-    // }

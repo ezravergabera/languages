ebd071338599b0ba106d5c46e4801d3e8504cc41
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-19 23:14:50 +0800

    My attempt at the react button component challenge

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from './components/Alert'
+import Button from './components/Button'
 
 function App() {
   return (
@@ -6,6 +7,31 @@ function App() {
       <Alert>
         Hello <span>World</span>
       </Alert>
+
+      <Button btnType='primary'>
+        primary testing
+      </Button>
+      <Button btnType='secondary'>
+        secondary testing
+      </Button>
+      <Button btnType='success'>
+        success testing
+      </Button>
+      <Button btnType='danger'>
+        danger testing
+      </Button>
+      <Button btnType='warning'>
+        warning testing
+      </Button>
+      <Button btnType='info'>
+        info testing
+      </Button>
+      <Button btnType='light'>
+        light testing
+      </Button>
+      <Button btnType='dark'>
+        dark testing
+      </Button>
     </div>
   );
 }

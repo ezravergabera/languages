f253347239e10cf9b23e07330f2e1b50261e7a43
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-20 20:27:28 +0800

    variables

--- website/index.js
+++ website/index.js
@@ -1,8 +1,31 @@
-console.log(`Hello`);
-console.log(`I like pizza!`);
+//! basics of javascript
+// console.log(`Hello`);
+// console.log(`I like pizza!`);
 
-window.alert(`This is an alert!`)
-window.alert(`I like pizza!`)
+// window.alert(`This is an alert!`)
+// window.alert(`I like pizza!`)
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza!`;
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I like pizza!`;
+
+
+//! variables
+// number data types
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+// string data types
+let firstName = "Ezra";
+let lastName = "Vergabera";
+
+// boolean data types
+let online = true;
+
+console.log(typeof age)
+console.log(typeof firstName)
+console.log(typeof online)
+console.log(`You are ${age} years old`);
+console.log(`The price is P${price}`); 
+console.log(`Your gpa is: ${gpa}`); 
+console.log(`Your name is ${firstName} ${lastName}`)

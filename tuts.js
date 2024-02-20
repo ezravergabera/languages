e4efc224cf96b4061a013144aed42a8f71e188d2
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-20 20:08:26 +0800

    simple js code needed for react

---
+++ tuts.js
@@ -0,0 +1,20 @@
+// Arrow Functions
+function DoSomething() {
+
+}
+
+const DoSomething = () => {
+
+}
+
+const MyComponent = () => {
+    return <div> </div>
+}
+
+<button onClick={() => {
+    console.log("Hello World!");
+    }}>
+
+</button>
+
+// Ternary Operators

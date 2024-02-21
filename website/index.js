5fbf6457e94469f2bcf07bf575bfaa8b57121ff8
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-21 17:12:28 +0800

    Practiced the Math object in javascript

--- website/index.js
+++ website/index.js
@@ -1,24 +1,45 @@
-// Counter Program
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
+//! MATH object
+// let x = 3.1415;
+// let y = 2;
+// let z = 1;
 
-increaseBtn.onclick = function() {
-    count++;
-    countLabel.textContent = count;
-}
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x, y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+// let max = Math.max(x, y, z);
+// let min = Math.min(x, y, z);
 
-decreaseBtn.onclick = function() {
-    count--;
-    countLabel.textContent = count;
-}
 
-resetBtn.onclick = function() {
-    count = 0;
-    countLabel.textContent = count;
-}
+//! Counter Program
+// const decreaseBtn = document.getElementById("decreaseBtn");
+// const resetBtn = document.getElementById("resetBtn");
+// const increaseBtn = document.getElementById("increaseBtn");
+// const countLabel = document.getElementById("countLabel");
+// let count = 0;
+
+// increaseBtn.onclick = function() {
+//     count++;
+//     countLabel.textContent = count;
+// }
+
+// decreaseBtn.onclick = function() {
+//     count--;
+//     countLabel.textContent = count;
+// }
+
+// resetBtn.onclick = function() {
+//     count = 0;
+//     countLabel.textContent = count;
+// }
 
 
 //! basics of javascript

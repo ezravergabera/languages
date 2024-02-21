36e453d57fe533d2bad7c55757b870fcafaf13b0
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-21 17:03:59 +0800

    created a counter program

--- website/index.js
+++ website/index.js
@@ -1,3 +1,26 @@
+// Counter Program
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+}
+
+
 //! basics of javascript
 // console.log(`Hello`);
 // console.log(`I like pizza!`);
@@ -39,9 +62,10 @@
 //     document.getElementById("myH1").textContent = username ? `Welcome ${username}!` : `Welcome`;
 // }
 
-//! type conversion
-let age = window.prompt("How old are you?");
-age = Number(age);
-age+=1;
 
-console.log(age);
+//! type conversion
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age+=1;
+
+// console.log(age);

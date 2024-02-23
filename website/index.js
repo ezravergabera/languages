81a578a3ecdc9d894fb25592db932d24e331d711
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-23 17:02:00 +0800

    Practiced map method

--- website/index.js
+++ website/index.js
@@ -1,3 +1,13 @@
+//! map() is like for each but returns a new array
+const numbers = [1, 2, 3, 4, 5];
+
+const squares = numbers.map(square);
+console.log(squares);
+
+function square(element) {
+    return Math.pow(element, 2);
+}
+
 //! forEach()
 // let numbers = [1, 2, 3, 4, 5];
 
@@ -24,18 +34,18 @@
 //     console.log(element);
 // }
 
-let fruits = ["apple", "orange", "banana", "coconut"];
+// let fruits = ["apple", "orange", "banana", "coconut"];
 
-fruits.forEach(upperCase);
-fruits.forEach(display);
+// fruits.forEach(upperCase);
+// fruits.forEach(display);
 
-function upperCase(element, index, array) {
-    array[index] = element.toUpperCase();
-}
+// function upperCase(element, index, array) {
+//     array[index] = element.toUpperCase();
+// }
 
-function display(element) {
-    console.log(element);
-}
+// function display(element) {
+//     console.log(element);
+// }
 
 
 //! callback

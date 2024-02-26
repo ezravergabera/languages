ac63dce4547e4e595a2b6e59f2a7447a795b12b2
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-26 19:47:27 +0800

    Practiced css styles in react

--- apps/my-react-app/src/App.jsx
+++ apps/my-react-app/src/App.jsx
@@ -2,13 +2,12 @@ import Header from './Header.jsx'
 import Food from './Food.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import Button from './Button/Button.jsx'
 
 function App() {
   return (
     <>
-       <Card/>
-       <Card/>
-       <Card/>
+       <Button/>
     </>
   );
 }

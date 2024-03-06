e3b8b02cd2e007f59e4e181beb43de389595bd21
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-03-06 18:23:22 +0800

    Practiced conditional rendering in react js

--- apps/my-react-app/src/App.jsx
+++ apps/my-react-app/src/App.jsx
@@ -4,13 +4,12 @@ import Footer from './Footer.jsx'
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx'
 import Student from './Student.jsx'
+import UserGreeting from './UserGreeting.jsx'
 
 function App() {
   return (
     <>
-       <Student name="Ezra" age={21} isStudent={true}/>
-       <Student name="Noob" age={43} isStudent={false}/>
-       <Student />
+       <UserGreeting isLoggedIn={true} username="ezravergabera"/>
     </>
   );
 }

fd95a0ecca71937d513e899bb83e6bb4a7a34cff
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-03-08 17:04:27 +0800

    Practiced rendering lists in react

--- apps/my-react-app/src/App.jsx
+++ apps/my-react-app/src/App.jsx
@@ -5,11 +5,12 @@ import Card from './Card.jsx'
 import Button from './Button/Button.jsx'
 import Student from './Student.jsx'
 import UserGreeting from './UserGreeting.jsx'
+import List from './List.jsx'
 
 function App() {
   return (
     <>
-       <UserGreeting isLoggedIn={true} username="ezravergabera"/>
+       <List/>
     </>
   );
 }

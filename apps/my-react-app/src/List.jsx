73518cae92bef2c2daa0233203388a10e905e770
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-03-10 20:00:34 +0800

    practiced filtering lists

--- apps/my-react-app/src/List.jsx
+++ apps/my-react-app/src/List.jsx
@@ -5,9 +5,18 @@ const List = () => {
                     {id: 4, name: "coconut", calories: 159}, 
                     {id: 5, name: "pineapple", calories: 37}];
 
-    fruits.sort((a, b) => );
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)); //ALPHABETICAL
+    // fruits.sort((a, b) => b.name.localeCompare(a.name)); //REVERSE ALPHABETICAL
+    // fruits.sort((a, b) => a.calories - b.calories); //NUMERIC
+    // fruits.sort((a, b) => b.calories - a.calories); //REVERSE NUMERIC
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name} - {fruit.calories}</li>);
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    // const listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>{lowCalFruit.name} - {lowCalFruit.calories}</li>)
+
+    const highCalFruits = fruits.filter(fruit => fruit.calories > 100);
+    const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>{highCalFruit.name} - {highCalFruit.calories}</li>)
+
+    // const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name} - {fruit.calories}</li>);
 
     return(<ol>{listItems}</ol>);
 }

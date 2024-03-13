e481fe9a84658732fb990ffc7997d13010f2fe2f
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-03-13 21:54:49 +0800

    Created a simple Counter

--- apps/my-react-app/src/App.jsx
+++ apps/my-react-app/src/App.jsx
@@ -8,11 +8,12 @@ import UserGreeting from './UserGreeting.jsx'
 import List from './List.jsx'
 import ProfilePicture from './ProfilePicture.jsx'
 import MyComponent from './MyComponent.jsx'
+import Counter from './Counter.jsx'
 
 function App() {
   return (
     <>
-      <MyComponent></MyComponent>
+      <Counter/>
     </>
   );
 }

e711893e6a4eea6ef61f9e62d482029615fa9cda
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-03-14 19:45:29 +0800

    Practiced onChange event handler

--- apps/my-react-app/src/App.jsx
+++ apps/my-react-app/src/App.jsx
@@ -9,11 +9,12 @@ import List from './List.jsx'
 import ProfilePicture from './ProfilePicture.jsx'
 import MyComponent from './MyComponent.jsx'
 import Counter from './Counter.jsx'
+import MyComponent2 from './MyComponent2.jsx'
 
 function App() {
   return (
     <>
-      <Counter/>
+      <MyComponent2/>
     </>
   );
 }

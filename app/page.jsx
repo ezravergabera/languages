0638311a201f2ab7f1d3b3874b6d4fbd544b08b8
Jose Ezra Nazarene Vergabera <ezra.vergabera@gmail.com>
2026-03-04 00:41:53 +0800

    Chapter 7: adding interactivity; queueing a series of state updates

--- app/page.jsx
+++ app/page.jsx
@@ -1,61 +1,75 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Header({ title }) {
-    console.log(title);
-    return <>
-        <h1>Hello World!</h1>
-        <h2>Playing with {title ? title : "Nothing"}</h2>
-    </>;
+  console.log(title);
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>Playing with {title ? title : 'Nothing'}</h2>
+    </>
+  );
 }
-    
+
 function Body(prop) {
-    console.log("this is a prop: " + prop.body);
-    return <>
-        <h1>{prop.body}</h1>
-        <h1>{`Template Literal Example: ${prop.body}`}</h1>
-    </>;
+  console.log('this is a prop: ' + prop.body);
+  return (
+    <>
+      <h1>{prop.body}</h1>
+      <h1>{`Template Literal Example: ${prop.body}`}</h1>
+    </>
+  );
 }
 
 function createTitle(title) {
-    if (title) {
-        return title;
-    } else {
-        return 'Default Title';
-    }
+  if (title) {
+    return title;
+  } else {
+    return 'Default Title';
+  }
 }
 
 export default function HomePage() {
-    const names = ["Arjie", "Ezra", "Fiona"];
-    const [likes, setLikes] = useState(0);
+  const names = ['Arjie', 'Ezra', 'Fiona'];
+  const [likes, setLikes] = useState(0);
 
-    function incrementLikeCounter() {
-        setLikes(likes + 1);
+  useEffect(() => {
+    if (likes%10 == 0 && likes != 0) {
+      alert(`Congratulations, you got ${likes}!`)
     }
+  }, [likes]);
 
-    function decrementLikeCounter() {
-        if (likes != 0) {
-            setLikes(likes - 1);
-        }
+  function incrementLikeCounter() {
+    setLikes(n => n + 1);
+  }
+
+  function decrementLikeCounter() {
+    if (likes != 0) {
+      setLikes(likes - 1);
     }
+  }
 
-    return (
-        <html>
-            <Header title="React"/>
-            <Header/>
-            <Body body="Hello World Body!"/>
-            <Body body={
-                <ul>
-                    {names.map((name) => (
-                        <li key={name}>{name}</li>
-                    ))}
-                </ul>
-            }/>
-            <h1>Likes: {likes}</h1>
-            <button onClick={incrementLikeCounter}>Like</button>
-            <button onClick={decrementLikeCounter}>Unlike</button>
-            <hr/>
-            <Header title={createTitle("Title from a Function")}/>
-        </html>
-    );
+  return (
+    <html>
+      <body>
+        <Header title='React' />
+        <Body body='Hello World Body!' />
+        <Header />
+        <Body
+          body={
+            <ul>
+              {names.map((name) => (
+                <li key={name}>{name}</li>
+              ))}
+            </ul>
+          }
+        />
+        <h1>Likes: {likes}</h1>
+        <button onClick={incrementLikeCounter}>Like</button>
+        <button onClick={decrementLikeCounter}>Unlike</button>
+        <hr />
+        <Header title={createTitle('Title from a Function')} />
+      </body>
+    </html>
+  );
 }

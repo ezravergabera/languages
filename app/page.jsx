85d69b589779b172b13a726d3a3b571916ca837c
Jose Ezra Nazarene Vergabera <ezra.vergabera@gmail.com>
2026-03-03 23:42:35 +0800

    build: added a .prettierrc file

---
+++ app/page.jsx
@@ -0,0 +1,61 @@
+'use client';
+import { useState } from 'react';
+
+function Header({ title }) {
+    console.log(title);
+    return <>
+        <h1>Hello World!</h1>
+        <h2>Playing with {title ? title : "Nothing"}</h2>
+    </>;
+}
+    
+function Body(prop) {
+    console.log("this is a prop: " + prop.body);
+    return <>
+        <h1>{prop.body}</h1>
+        <h1>{`Template Literal Example: ${prop.body}`}</h1>
+    </>;
+}
+
+function createTitle(title) {
+    if (title) {
+        return title;
+    } else {
+        return 'Default Title';
+    }
+}
+
+export default function HomePage() {
+    const names = ["Arjie", "Ezra", "Fiona"];
+    const [likes, setLikes] = useState(0);
+
+    function incrementLikeCounter() {
+        setLikes(likes + 1);
+    }
+
+    function decrementLikeCounter() {
+        if (likes != 0) {
+            setLikes(likes - 1);
+        }
+    }
+
+    return (
+        <html>
+            <Header title="React"/>
+            <Header/>
+            <Body body="Hello World Body!"/>
+            <Body body={
+                <ul>
+                    {names.map((name) => (
+                        <li key={name}>{name}</li>
+                    ))}
+                </ul>
+            }/>
+            <h1>Likes: {likes}</h1>
+            <button onClick={incrementLikeCounter}>Like</button>
+            <button onClick={decrementLikeCounter}>Unlike</button>
+            <hr/>
+            <Header title={createTitle("Title from a Function")}/>
+        </html>
+    );
+}

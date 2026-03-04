028e26b3e723c2fc4866081877c19f4887488fb1
Jose Ezra Nazarene Vergabera <ezra.vergabera@gmail.com>
2026-03-04 23:55:32 +0800

    Chapter 9: Pages and Layouts

--- app/page.jsx
+++ app/page.jsx
@@ -1,75 +1,28 @@
-'use client';
-import { useEffect, useState } from 'react';
-
+import { useState } from 'react';
+ 
 function Header({ title }) {
-  console.log(title);
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <h2>Playing with {title ? title : 'Nothing'}</h2>
-    </>
-  );
+  return <h1>{title ? title : 'Default title'}</h1>;
 }
-
-function Body(prop) {
-  console.log('this is a prop: ' + prop.body);
-  return (
-    <>
-      <h1>{prop.body}</h1>
-      <h1>{`Template Literal Example: ${prop.body}`}</h1>
-    </>
-  );
-}
-
-function createTitle(title) {
-  if (title) {
-    return title;
-  } else {
-    return 'Default Title';
-  }
-}
-
+ 
 export default function HomePage() {
-  const names = ['Arjie', 'Ezra', 'Fiona'];
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+ 
   const [likes, setLikes] = useState(0);
-
-  useEffect(() => {
-    if (likes%10 == 0 && likes != 0) {
-      alert(`Congratulations, you got ${likes}!`)
-    }
-  }, [likes]);
-
-  function incrementLikeCounter() {
-    setLikes(n => n + 1);
+ 
+  function handleClick() {
+    setLikes(likes + 1);
   }
-
-  function decrementLikeCounter() {
-    if (likes != 0) {
-      setLikes(likes - 1);
-    }
-  }
-
+ 
   return (
-    <html>
-      <body>
-        <Header title='React' />
-        <Body body='Hello World Body!' />
-        <Header />
-        <Body
-          body={
-            <ul>
-              {names.map((name) => (
-                <li key={name}>{name}</li>
-              ))}
-            </ul>
-          }
-        />
-        <h1>Likes: {likes}</h1>
-        <button onClick={incrementLikeCounter}>Like</button>
-        <button onClick={decrementLikeCounter}>Unlike</button>
-        <hr />
-        <Header title={createTitle('Title from a Function')} />
-      </body>
-    </html>
+    <div>
+      <Header title="Develop. Preview. Ship." />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+ 
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
   );
 }
